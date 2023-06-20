b38f0b0098bf536f3e8c4dcc5ca0b62db9866ca8
Rohit Gupta <65344659+Rohit4352@users.noreply.github.com>
2023-06-20 10:48:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+var txt ;
+txt = "You are using " + navigator.appName;
+txt += " version " + navigator.appVersion;
+
+document.getElementById("browser-info").innerHTML = txt;
